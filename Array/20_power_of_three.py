c75747333e2020950093a9feace29d701bb2f5b7
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-01-12 09:54:07 -0800

    Add files via upload

---
+++ Array/20_power_of_three.py
@@ -0,0 +1,41 @@
+# ============================================================================
+# Name        : 20_power_of_three.py
+# Author      : Saranya Balakrishnan
+# Mail Id     : saranyab0925@gmail.com
+# ============================================================================
+"""
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+Follow up:
+Could you do it without using any loop / recursion?
+"""
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 0:
+            return False
+        elif 1162261467 % n == 0:
+          return True
+        else:
+            return False
+if __name__ == '__main__':
+    integer = Solution()
+    n = 9
+    res = integer.isPowerOfThree(n)
+    print(res)
