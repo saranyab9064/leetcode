e44f98437a297dbd1e509ba448e04e2a3c45afdf
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-01-18 08:56:17 -0800

    Add files via upload

---
+++ Strings/13_right_angle_tri_pattern_with_fixed_alphabet.py
@@ -0,0 +1,8 @@
+# ============================================================================
+# Name        : 10_inverted_right_angle_tri_pattern.py
+# Author      : Saranya Balakrishnan
+# Mail Id     : saranyab0925@gmail.com
+# ============================================================================
+n = 8
+for i in range(n):
+    print((chr(65+i)*(i+1)))
