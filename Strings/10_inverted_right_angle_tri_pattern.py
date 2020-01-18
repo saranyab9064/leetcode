dec9dc2031c4905730a1b3a9c884ce72d3754df9
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-01-18 08:41:25 -0800

    Add files via upload

---
+++ Strings/10_inverted_right_angle_tri_pattern.py
@@ -0,0 +1,8 @@
+# ============================================================================
+# Name        : 10_inverted_right_angle_tri_pattern.py
+# Author      : Saranya Balakrishnan
+# Mail Id     : saranyab0925@gmail.com
+# ============================================================================
+string = input("enter the string:")
+for i in range(len(string)+1):
+        print(string[0:len(string)-i])
