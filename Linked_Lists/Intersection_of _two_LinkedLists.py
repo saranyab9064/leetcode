5a32ce4c7f7dbaf36a60462c8fcd10a961026069
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2020-07-25 23:52:56 -0700

    Update Intersection_of _two_LinkedLists.py

--- Linked_Lists/Intersection_of _two_LinkedLists.py
+++ Linked_Lists/Intersection_of _two_LinkedLists.py
@@ -10,13 +10,15 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+        # initialise two pointers tempA and tempB
         if (headA == None or headB == None):
             return None
         tempA = headA
         tempB = headB
         lenA = 0
         lenB = 0
-        
+        # find the length of two lists 
+        # to check whether the two lists has same length or not
         while tempA.next != None:
             lenA = lenA +1
             tempA = tempA.next
@@ -24,7 +26,7 @@ class Solution(object):
         while tempB.next != None:
             lenB = lenB +1
             tempB = tempB.next   
-        
+        # to find which list has larger length and assigning the larger length to tempA
         diff = 0
         if lenA > lenB:
             tempA = headA
@@ -34,12 +36,15 @@ class Solution(object):
             tempA = headB
             tempB = headA
             diff = lenB - lenA  
+           
         while diff > 0:
             tempA = tempA.next
             diff = diff - 1
+            # iterate each element (larger list as A with smaller one as B)
         while tempA != tempB:
             tempA = tempA.next
             tempB = tempB.next
+            
         return tempA
         
         
