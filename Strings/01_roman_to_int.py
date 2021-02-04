9146e45ea96eaedb896e7b8ee9a99f050c924406
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2021-02-03 22:47:50 -0800

    Update 01_roman_to_int.py

--- Strings/01_roman_to_int.py
+++ Strings/01_roman_to_int.py
@@ -63,7 +63,17 @@ class Solution(object):
              res += rom_val[s[i]]
         return res
 
-
+"""
+        res = 0
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for i in  range(0,len(s)):
+            print(i)
+            if i+1!=len(s) and (rom_val[s[i]] < rom_val[s[i+1]]):
+                res -= rom_val[s[i]] 
+            else:
+             res += rom_val[s[i]]
+        return res
+"""        
 if __name__ == '__main__':
     s = "MCMXCIV"
     integer = Solution()
