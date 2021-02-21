640e3e66a9a30a834032909c56fb27c89064140e
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2021-02-20 20:47:27 -0800

    Create 1684_Count_the_Number_of_Consistent_Strings.py

---
+++ Strings/1684_Count_the_Number_of_Consistent_Strings.py
@@ -0,0 +1,59 @@
+
+'''
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ 
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+'''
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        def helper(word,allow):
+            flag = False
+            for i in range(len(word)):
+                if word[i] not in allow:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            return flag
+        out = []   
+        for i in range(len(words)):
+            out.append(helper(words[i],allowed))
+        print(out)
+        count = 0
+        for i in out:
+            if i:
+                count = count + 1
+        return count
+        
