daead7105d60c488c7a3dbd0bc2ac8241a274ac4
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2021-03-04 21:29:09 -0800

    Create insertNodeAtHead.py

---
+++ Hackerrank/Linked_List/insertNodeAtHead.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(head, data):
+    # Write your code here
+    node = head
+    if head is None:
+        head = SinglyLinkedListNode(data)
+        return head
+    else:
+        temp = head
+        head = SinglyLinkedListNode(data)
+        head.next = temp
+        return head
+
+if __name__ == '__main__':
