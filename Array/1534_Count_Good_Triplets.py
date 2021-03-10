ca6ffffcb775c4caacc4dc907b9912b40a48a343
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2021-03-09 17:49:55 -0800

    Create 1534_Count_Good_Triplets.py

---
+++ Array/1534_Count_Good_Triplets.py
@@ -0,0 +1,59 @@
+'''
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+
+ 
+
+Example 1:
+
+Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+Output: 4
+Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+Example 2:
+
+Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+Output: 0
+Explanation: No triplet satisfies all conditions.
+ 
+
+Constraints:
+
+3 <= arr.length <= 100
+0 <= arr[i] <= 1000
+0 <= a, b, c <= 1000
+'''
+class Solution(object):
+    def countGoodTriplets(self, arr, a, b, c):
+        """
+        :type arr: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                for k in range(j+1,len(arr)):
+                    if abs(arr[i] - arr[j]) <=a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k])<= c:
+                         count += 1
+        return count
+         
+
+test = Solution()
+n = [7,3,7,3,12,1,12,2,3]
+a = 5
+b = 8
+c = 1
+
+out = test.countGoodTriplets(n,a,b,c)
+print(out)
