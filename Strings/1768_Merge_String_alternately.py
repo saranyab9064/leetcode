70ed663e65c3c730911ba2b8acf0c710c270eefb
Saranya Balakrishnan <55033431+saranyab9064@users.noreply.github.com>
2021-03-09 17:28:49 -0800

    Create 1768_Merge_String_alternately.py

---
+++ Strings/1768_Merge_String_alternately.py
@@ -0,0 +1,76 @@
+'''
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+'''
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        l1 = len(word1)
+        l2 = len(word2)
+        i = 0
+        j = 0
+        res = ""
+        if l1 < l2:
+           while i < l1 or j < l2:
+                if i <l1:
+                    res = res + word1[i]
+                    i = i + 1
+                if j <l2:
+                    res = res + word2[j]
+                    j= j + 1 
+        else:
+             while i < l1 or j < l2:
+                if i <l1:
+                    res = res + word1[i]
+                    i = i + 1
+                if j <l2:
+                    res = res + word2[j]
+                    j= j + 1                 
+        return(res)
+test = Solution()
+w1 = "abcd"
+w2 = "pq"
+out=test.mergeAlternately(w1,w2)
+print(out)
+
+
+
